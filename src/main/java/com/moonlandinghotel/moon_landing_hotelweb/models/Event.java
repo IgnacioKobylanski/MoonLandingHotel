2f2e768c54102c0b755060a5a266c3595b969df2
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-12-06 18:36:44 -0300

    Event class created

--- src/main/java/com/moonlandinghotel/moon_landing_hotelweb/models/Event.java
+++ src/main/java/com/moonlandinghotel/moon_landing_hotelweb/models/Event.java
@@ -2,19 +2,26 @@ package com.moonlandinghotel.moon_landing_hotelweb.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import java.util.Set;
 
 @Data
 @Entity
-public class Room {
+public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     private String name;
 
-    private String price;
-
     private String description;
 
-    private String imageUrl;
+    private String text;
+
+    private double price;
+
+    private int dateD;
+
+    private String dateM;
+
+    private String img;
 }
--- src/main/java/com/moonlandinghotel/moon_landing_hotelweb/models/User.java
+++ src/main/java/com/moonlandinghotel/moon_landing_hotelweb/models/User.java
@@ -1,27 +1,26 @@
 package com.moonlandinghotel.moon_landing_hotelweb.models;
 
-import com.moonlandinghotel.moon_landing_hotelweb.models.enums.Role;
 import jakarta.persistence.*;
 import lombok.Data;
-
 import java.util.Set;
 
 @Data
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String email;
+
     @Column(length = 60)
     private String password;
 
-    @ElementCollection(targetClass = Role.class)
-    @Enumerated(EnumType.STRING)
-    private Set<Role> roles;
+    private String name;
 
+    private String lastname;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private Set<Reservation> reservations;
+    private boolean admin;
+
 }
